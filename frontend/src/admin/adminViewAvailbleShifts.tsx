fb0782dd9878707304f7819a76e5ac912ea94e5d
Fahmid99 <fahmidahmed024@gmail.com>
2022-09-07 03:07:58 +1000

    form update

--- frontend/src/admin/adminViewAvailbleShifts.tsx
+++ frontend/src/admin/adminViewAvailbleShifts.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Component } from "react";
 import { NavigationBar } from "../components/navbar";
 import { AvailableShiftsBtn } from "../components/availableShiftsBtn";
 import AddShiftForm from "../components/addShiftForm";
@@ -8,58 +6,86 @@ import addShiftIcon from "../assets/addShiftIcon.svg";
 import deleteIcon from "../assets/deleteIcon.svg";
 import filterIcon from "../assets/filterIcon.svg";
 import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
 
-export class AdminViewAvailbleShifts extends Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-            show: false,
-        };
-    }
-    openAddShift = () => {
-        this.setState({ show: true });
+const defaultFormFields = {
+    shiftTitle: "",
+    shiftDescription: "",
+    startDate: "",
+    endDate: "",
+    shiftTime: "",
+    shiftAddress: "",
+    shiftVenue: "",
+    addressDescription: "",
+    shiftHours: " ",
+};
+
+export const AdminViewAvailbleShifts = () => {
+    const [show, setShow] = useState(false);
+    const [formFields, setFormFields] = useState(defaultFormFields);
+
+    // const {
+    //     shiftTitle,
+    //     shiftDescription,
+    //     startDate,
+    //     endDate,
+    //     shiftTime,
+    //     shiftAddress,
+    //     shiftVenue,
+    //     addressDescription,
+    //     shiftHours,
+    // } = formFields;
+
+    const handleChange = (event: { target: { name: any; value: any } }) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const { name, value } = event.target;
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        setFormFields({ ...formFields, [name]: value });
+        console.log(formFields);
+    };
+
+    const openAddShift = () => {
+        setShow(true);
         console.log("button1");
     };
 
-    deleteSelected = () => {
+    const deleteSelected = () => {
         console.log("button2");
     };
 
-    handleFilter = () => {
+    const handleFilter = () => {
         console.log("button3");
     };
 
-    render() {
-        return (
-            <div>
-                <NavigationBar />
-                <div className="header-container">
-                    <h1>Available Shifts</h1>
-                    <div className="btn-container">
-                        <AvailableShiftsBtn
-                            className="admin-btn"
-                            btnText="Add Shift"
-                            btnIcon={addShiftIcon}
-                            onClickHandler={this.openAddShift}
-                        />
-                        <AvailableShiftsBtn
-                            className="admin-btn"
-                            btnText="Delete Selected"
-                            btnIcon={deleteIcon}
-                            onClickHandler={this.deleteSelected}
-                        />
-                        <AvailableShiftsBtn
-                            className="admin-btn"
-                            btnText="Filters"
-                            btnIcon={filterIcon}
-                            onClickHandler={this.handleFilter}
-                        />
-                    </div>
+    return (
+        <div>
+            <NavigationBar />
+            <div className="header-container">
+                <h1>Available Shifts</h1>
+                <div className="btn-container">
+                    <AvailableShiftsBtn
+                        className="admin-btn"
+                        btnText="Add Shift"
+                        btnIcon={addShiftIcon}
+                        onClickHandler={openAddShift}
+                    />
+                    <AvailableShiftsBtn
+                        className="admin-btn"
+                        btnText="Delete Selected"
+                        btnIcon={deleteIcon}
+                        onClickHandler={deleteSelected}
+                    />
+                    <AvailableShiftsBtn
+                        className="admin-btn"
+                        btnText="Filters"
+                        btnIcon={filterIcon}
+                        onClickHandler={handleFilter}
+                    />
                 </div>
-                <Modal show={this.state.show}>
-                    <AddShiftForm />
-                </Modal>
             </div>
-        );
-    }
-}
+            <Modal show={show}>
+                <AddShiftForm handleEvent={handleChange} />
+            </Modal>
+        </div>
+    );
+};
--- frontend/src/components/addShiftForm.tsx
+++ frontend/src/components/addShiftForm.tsx
@@ -1,35 +1,41 @@
-import "./addShiftForm.css";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-const AddShiftForm = () => {
+import "./addShiftForm.css";
+type formProps = {
+    handleEvent: any;
+    // ...rest of your props
+};
+
+const AddShiftForm: React.FC<formProps> = ({ handleEvent }) => {
     return (
         <div>
             <form className="add-shift-form">
                 <label>Title</label>
-                <input type="text" required name="shiftTitle" />
+                <input type="text" required name="shiftTitle" onChange={handleEvent} />
 
                 <label>Description</label>
-                <input type="text" required name="shiftDescription" />
+                <textarea required name="shiftDescription" onChange={handleEvent} />
 
                 <label>Select Start Date</label>
-                <input type="date" required name="startDate" />
+                <input type="date" required name="startDate" onChange={handleEvent} />
 
                 <label>Select End Date</label>
-                <input type="date" required name="endDate" />
+                <input type="date" required name="endDate" onChange={handleEvent} />
 
                 <label>Time</label>
-                <input type="text" required name="shiftDescription" />
+                <input type="text" required name="shiftTime" onChange={handleEvent} />
 
                 <label>Address</label>
-                <input type="text" required name="shiftDescription" />
+                <input type="text" required name="shiftAddress" onChange={handleEvent} />
 
                 <label>Venue</label>
-                <input type="text" required name="shiftDescription" />
+                <input type="text" required name="shiftVenue" onChange={handleEvent} />
 
                 <label>Address Description</label>
-                <input type="text" required name="shiftDescription" />
+                <input type="text" required name="addressDescription" onChange={handleEvent} />
 
                 <label>Hours</label>
-                <input type="text" required name="shiftDescription" />
+                <input type="text" required name="shiftHours" onChange={handleEvent} />
             </form>
         </div>
     );
