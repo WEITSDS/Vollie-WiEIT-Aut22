d865aaae769ef343c46aab282fb2a2b9fb29efa4
nath1100 <nath.luu1@gmail.com>
2022-09-07 20:18:10 +1000

    Adjusted styling and added icon SVGs for shift cards

--- frontend/src/components/shiftCard.tsx
+++ frontend/src/components/shiftCard.tsx
@@ -1,18 +1,29 @@
 import { Button, Card, Stack } from "react-bootstrap";
+import participantsIcon from "../assets/participants.svg";
+import locationIcon from "../assets/location.svg";
+import calendarIcon from "../assets/calendar.svg";
 
-export default function ShiftCard() {
+export default function ShiftCard({ shiftName = "Shift Name", location = "Location info", date = "Date text" }) {
     return (
-        <Card style={{ width: "20rem", borderRadius: "1rem" }}>
+        <Card className="shadow p-3 mb-5 bg-body" style={{ width: "25rem", borderRadius: "1rem" }}>
             <Card.Body>
-                <Stack direction="horizontal" gap={5}>
-                    <Card.Title>Shift Name Here</Card.Title>
-                    <p className="ms-auto">ICON</p>
+                <Stack direction="horizontal" style={{ justifyContent: "space-between" }}>
+                    <Card.Title>{shiftName}</Card.Title>
+                    <Button size="sm" variant="light" style={{ borderRadius: "50%" }}>
+                        <img src={participantsIcon} alt="participants icon" />
+                    </Button>
                 </Stack>
-                <Card.Text>Location info here</Card.Text>
+                <Card.Text as="h6" style={{ color: "blue" }}>
+                    <img src={locationIcon} alt="location icon" />
+                    {location}
+                </Card.Text>
                 <hr />
                 <Stack direction="horizontal" style={{ justifyContent: "space-between" }}>
-                    <Card.Text>Date info here</Card.Text>
-                    <Button style={{ borderRadius: "4rem" }}>View</Button>
+                    <Card.Text as="h6">
+                        <img src={calendarIcon} alt="date icon" />
+                        {date}
+                    </Card.Text>
+                    <Button style={{ borderRadius: "4rem", padding: "0.5rem 1.5rem" }}>View</Button>
                 </Stack>
             </Card.Body>
         </Card>
