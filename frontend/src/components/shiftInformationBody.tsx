6a57fbd81d5b478022526bad2c5ae649572f6b70
LNegus <111871721+LNegus@users.noreply.github.com>
2022-09-11 14:34:24 +1000

    Updated

--- frontend/src/components/shiftInformationBody.tsx
+++ frontend/src/components/shiftInformationBody.tsx
@@ -1,42 +1,71 @@
 import "./shiftInformationBody.css";
+import { Button } from "react-bootstrap";
 
 const ShiftInformationBody = () => {
     return (
         <body>
             <div>
                 <h1>Brief Description:</h1>
-                <p>Body</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
-            <div id="block">
-                <h1>Time:</h1>
+            <div>
+                <p>
+                    <strong>Time:</strong> 8am - 12pm
+                </p>
+                {/* <h2>8am-12pm</h2> */}
             </div>
-            <div id="block">
-                <h2>8am-12pm</h2>
+            {/* <div id="block">
+            </div> */}
+            <div>
+                <p>
+                    <strong>Venue:</strong> Orange High School
+                </p>
             </div>
-            <div></div>
-            <div id="block">
+            {/* <div id="block">
                 <h1>Venue:</h1>
             </div>
             <div id="block">
                 <h2>Orange High School</h2>
-            </div>
+            </div> */}
             <div>
                 <h1>Address Description</h1>
-                <p>Description</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                </p>
             </div>
-            <div id="block">
+            {/* <div id="block">
                 <h1>Volunteer Type:</h1>
             </div>
             <div id="block">
                 <h2>Teaching</h2>
+            </div> */}
+            <div>
+                <p>
+                    <strong>Volunteer Type:</strong> Teaching
+                </p>
             </div>
-            <div></div>
-            <div id="block">
+            {/* <div id="block">
                 <h1>Volunteer Numbers:</h1>
             </div>
             <div id="block">
                 <h2>2</h2>
+            </div> */}
+            <div>
+                <p>
+                    <strong>Volunteer Numbers:</strong> 2
+                </p>
             </div>
+            <Button>Cancel</Button>
         </body>
     );
 };
