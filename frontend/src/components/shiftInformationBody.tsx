2979d153d73b504d42f8e6a510f3be4676682a4b
LNegus <111871721+LNegus@users.noreply.github.com>
2022-09-11 13:51:35 +1000

    Shift information basic template

--- frontend/src/components/shiftInformationBody.tsx
+++ frontend/src/components/shiftInformationBody.tsx
@@ -1,7 +1,44 @@
 import "./shiftInformationBody.css";
 
 const ShiftInformationBody = () => {
-    return <h1>hello</h1>;
+    return (
+        <body>
+            <div>
+                <h1>Brief Description:</h1>
+                <p>Body</p>
+            </div>
+            <div id="block">
+                <h1>Time:</h1>
+            </div>
+            <div id="block">
+                <h2>8am-12pm</h2>
+            </div>
+            <div></div>
+            <div id="block">
+                <h1>Venue:</h1>
+            </div>
+            <div id="block">
+                <h2>Orange High School</h2>
+            </div>
+            <div>
+                <h1>Address Description</h1>
+                <p>Description</p>
+            </div>
+            <div id="block">
+                <h1>Volunteer Type:</h1>
+            </div>
+            <div id="block">
+                <h2>Teaching</h2>
+            </div>
+            <div></div>
+            <div id="block">
+                <h1>Volunteer Numbers:</h1>
+            </div>
+            <div id="block">
+                <h2>2</h2>
+            </div>
+        </body>
+    );
 };
 
 export default ShiftInformationBody;
