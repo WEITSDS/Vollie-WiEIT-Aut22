4168becb691232756912183ef8123b8529082eea
Fahmid99 <fahmidahmed024@gmail.com>
2022-09-15 23:42:49 +1000

    updated css for right box

--- frontend/src/shiftInformation/shiftInformation.tsx
+++ frontend/src/shiftInformation/shiftInformation.tsx
@@ -30,15 +30,15 @@ const ShiftInformation = () => {
         name,
         startAt,
         // endAt,
-        // address,
+        address,
         // status,
         description,
-        // hours,
-        // numGeneralVolunteers,
-        // numUndergradAmbassadors,
-        // numPostgradAmbassadors,
-        // numStaffAmbassadors,
-        // numSprouts,
+        hours,
+        numGeneralVolunteers,
+        numUndergradAmbassadors,
+        numPostgradAmbassadors,
+        numStaffAmbassadors,
+        numSprouts,
     } = data?.data || {};
 
     const dateStringStart = new Date(startAt).toUTCString();
@@ -48,7 +48,7 @@ const ShiftInformation = () => {
         <div className="page-background">
             <NavigationBar />
             {data.success && (
-                <div className="page-container">
+                <div className="shift-info-page-container">
                     <div className="left-box-container">
                         <div className="header-button-container">
                             <div className="header-flex">
@@ -83,7 +83,7 @@ const ShiftInformation = () => {
                                     </div>
 
                                     <div className="info-box-right-container">
-                                        <h3 className="info-body">Orange High School</h3>
+                                        <h3 className="info-body">{address}</h3>
                                     </div>
                                 </div>
 
@@ -101,11 +101,11 @@ const ShiftInformation = () => {
                                 <div className="info-box">
                                     <div className="info-box-left-container">
                                         <img className="timeIcon" src={timeIcon}></img>
-                                        <h2 className="info-title">Time</h2>
+                                        <h2 className="info-title">Hours</h2>
                                     </div>
 
                                     <div className="info-box-right-container">
-                                        <h3 className="info-body">Orange High School</h3>
+                                        <h3 className="info-body">{hours}</h3>
                                     </div>
                                 </div>
                             </div>
@@ -129,7 +129,50 @@ const ShiftInformation = () => {
                             <button className="cancel-shift-btn">Cancel shift</button>
                         </div>
                     </div>
-                    <div className="right-box-container"></div>
+                    <div className="right-box-container">
+                        <div className="header-right-box-container">
+                            <div className="header-flex">
+                                <h1 className="right-box-title">Volunteer allocations</h1>
+                                <button className="participants-btn">Participants</button>
+                            </div>
+                        </div>
+                        <hr className="right-box-header-divider" />
+                        <div className="volunteer-table-container">
+                            <table className="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Type</th>
+                                        <th scope="col">Available Slots</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr key={0}>
+                                        <td>General Volunteers</td>
+                                        <td>{numGeneralVolunteers}</td>
+                                    </tr>
+                                    <tr key={1}>
+                                        <td>Undergrad Ambassadors</td>
+                                        <td>{numUndergradAmbassadors}</td>
+                                    </tr>
+                                    <tr key={2}>
+                                        <td>Postgrad Ambassadors</td>
+                                        <td>{numPostgradAmbassadors}</td>
+                                    </tr>
+                                    <tr key={3}>
+                                        <td>Staff Ambassadors</td>
+                                        <td>{numStaffAmbassadors}</td>
+                                    </tr>
+                                    <tr key={4}>
+                                        <td>Sprouts</td>
+                                        <td>{numSprouts}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <hr className="table-divider" />
+                        <h1 className="calender-title">Calendar</h1>
+                        <hr className="calendar-title-divider" />
+                    </div>
                 </div>
             )}
         </div>
