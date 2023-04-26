457ce1c15a527367b389bcf48ec1d3a2df4d52e0
Danny-Vien <13898604@student.uts.edu.au>
2023-04-26 15:01:50 +1000

    Revert Comments

--- backend/src/Notifications/notifications.interface.ts
+++ backend/src/Notifications/notifications.interface.ts
@@ -2,8 +2,7 @@ import { Document, Types } from "mongoose";
 
 export interface INotification extends Document {
     content: string;
-    //user: Types.ObjectId;
-    user: string;
+    user: Types.ObjectId;
     time: Date;
 }
 
