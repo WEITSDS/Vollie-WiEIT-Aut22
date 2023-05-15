79579e5b99830ea240fd211d2c21f7a9cf733aa4
Danny-Vien <13898604@student.uts.edu.au>
2023-05-15 21:08:12 +1000

    Resolve Changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -39,7 +39,6 @@ function App(): JSX.Element {
                     <Route path="/login" element={<LoginPage />}></Route>
                     <Route path="/forgotpassword" element={<ResetPaswordForm redirectTo="/home" />}></Route>
                     <Route path="/register" element={<RegisterPage />}></Route>
-                    <></>
                     <Route
                         path="/search"
                         element={<ProtectedRoute needsAdmin={true} outlet={<AdminViewAllUsers />} />}
