9900618d853f384ff4ad826d59cf5323ea4e8c67
13204767 <13204767@student.uts.edu.au>
2023-10-24 02:41:27 +1100

    Multiple changes to the add shift form

--- frontend/src/components/attendanceListVol.tsx
+++ frontend/src/components/attendanceListVol.tsx
@@ -1,14 +1,9 @@
 import { useEffect, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import { IShift, setApprovalUserForShift, UserShiftAttendaceSummary } from "../api/shiftApi";
+import { IShift, UserShiftAttendaceSummary } from "../api/shiftApi";
 import participantsIcon from "../assets/participants.svg";
-//import AttendanceList from "./attendanceList.json";
 import { useAttendanceList } from "../hooks/useAttendanceList";
-import RemoveUserFromShiftModal from "./removeUserFromShiftModal";
-// import axios from "axios";
 import "./attendanceList.css";
-import { Link } from "react-router-dom";
-import { setCompleteShift } from "../api/userApi";
 
 type AttendanceListProps = {
     shift: IShift;
@@ -18,7 +13,7 @@ type AttendanceListProps = {
     onCloseModal?: () => void;
 };
 
-export default function AttendanceListModal({
+export default function AttendanceListVolModal({
     shift,
     showModal,
     setShowModal,
@@ -35,107 +30,11 @@ export default function AttendanceListModal({
         void refetch();
     }, [showModal, modalBox]);
 
-    const onDeleteUser = async () => {
-        await refetch();
-        if (onCloseModal) {
-            onCloseModal();
-        }
-    };
-
-    const onApproveUser = async (targetUserId: string) => {
-        try {
-            await setApprovalUserForShift(targetUserId, shift._id, "approve");
-            await refetch();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const onUnApproveUser = async (targetUserId: string) => {
-        try {
-            await setApprovalUserForShift(targetUserId, shift._id, "unapprove");
-            await refetch();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const onMarkAsComplete = async (targetUserId: string) => {
-        try {
-            await setCompleteShift(targetUserId, shift._id, "complete");
-            await refetch();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const onMarkAsIncomplete = async (targetUserId: string) => {
-        try {
-            await setCompleteShift(targetUserId, shift._id, "incomplete");
-            await refetch();
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
     const handleClosingThis = () => {
         if (onCloseModal) onCloseModal();
 
         if (setShowModal) setShowModal();
         else if (handleClose) handleClose();
-    };
-
-    const handleExport = () => {
-        const rows = [
-            [
-                "Shift ID",
-                "Name",
-                "Venue",
-                "Address",
-                "Start Date/Time",
-                "End Date/Time",
-                "Length (Hours)",
-                "Category",
-                "Description",
-                // "Notes",
-            ],
-            [
-                shift._id,
-                shift.name,
-                shift.venue,
-                shift.address,
-                shift.startAt,
-                shift.endAt,
-                shift.hours,
-                shift.category,
-                shift.description,
-                // shift.notes,
-            ],
-            [],
-            ["Volunteer ID", "First Name", "Last Name", "Volunteer Type", "Approval Status", "Completion Status"],
-        ] as string[][];
-        attendanceListUsers?.data?.forEach((userShift) =>
-            rows.push([
-                userShift._id,
-                userShift.firstName,
-                userShift.lastName,
-                userShift.volTypeName,
-                userShift.approved ? "Approved" : "Pending Approval",
-                userShift.completed ? "Completed" : "Pending Completion",
-            ] as string[])
-        );
-        const csvContent =
-            "data:text/csv;charset=utf-8," +
-            rows
-                .map((e) => e.map((str) => (typeof str === "string" ? str.replace(/,/g, "") : str)).join(","))
-                .join("\n");
-        const encodedUri = encodeURI(csvContent);
-        const link = document.createElement("a");
-        link.setAttribute("href", encodedUri);
-        link.setAttribute("download", `shift_${shift.name}_${shift._id}.csv`);
-        document.body.appendChild(link);
-
-        link.click();
     };
 
     //With typescript, we should be defining a type for the users variable because it's a non standard datatype.
@@ -173,37 +72,7 @@ export default function AttendanceListModal({
         return attendanceList?.map((userShift, index) => (
             <tr key={userShift._id}>
                 <th scope="row">{index + 1}</th>
-                <td>
-                    <Link to={`/profile/${userShift._id}`}>
-                        {userShift.firstName}&nbsp;{userShift.lastName}
-                    </Link>
-                </td>
                 <td>{userShift.volTypeName}</td>
-                <td>{userShift.approved ? "Approved" : "Not Approved"}</td>
-                <td>{userShift.completed ? "Completed" : "Not Completed"}</td>
-                <td>
-                    {!userShift.approved ? (
-                        <Button onClick={() => void onApproveUser(userShift._id)}>Approve</Button>
-                    ) : (
-                        <Button onClick={() => void onUnApproveUser(userShift._id)}>Unapprove</Button>
-                    )}
-                </td>
-                <td>
-                    {!userShift.completed ? (
-                        <Button onClick={() => void onMarkAsComplete(userShift._id)}>Mark Complete</Button>
-                    ) : (
-                        <Button onClick={() => void onMarkAsIncomplete(userShift._id)}>Mark Incomplete</Button>
-                    )}
-                </td>
-                <td>
-                    <RemoveUserFromShiftModal
-                        onDelete={() => {
-                            void onDeleteUser();
-                        }}
-                        shiftId={shift._id || ""}
-                        userId={userShift._id || ""}
-                    />
-                </td>
             </tr>
         ));
     };
@@ -254,7 +123,6 @@ export default function AttendanceListModal({
                     )}
                 </Modal.Body>
                 <Modal.Footer className="attendance-modal-footer">
-                    <Button onClick={handleExport}>Export</Button>
                     <Button onClick={handleClosingThis}>Close</Button>
                 </Modal.Footer>
             </Modal>
