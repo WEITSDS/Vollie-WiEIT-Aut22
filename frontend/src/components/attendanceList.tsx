49af135fe4384e8b2ca85fb8eea8deb79be524a8
kimh1235 <kimh1235@gmail.com>
2023-12-11 01:49:03 +1100

    fix

--- frontend/src/components/attendanceList.tsx
+++ frontend/src/components/attendanceList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { useEffect, useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { IShift, setApprovalUserForShift, UserShiftAttendaceSummary } from "../api/shiftApi";
