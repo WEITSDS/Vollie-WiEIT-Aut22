65cdfe035ca12e082f9cb0726b4568b644b61533
steph1er <stephanie.l.roberts@student.uts.edu.au>
2024-04-28 13:48:21 +1000

    Added feedback forms page completed/pending sections

--- frontend/src/feedbackFormPage/feedbackFormPage.tsx
+++ frontend/src/feedbackFormPage/feedbackFormPage.tsx
@@ -4,10 +4,13 @@ import { NavigationBar } from "../components/navbar";
 import { WEITBackground } from "../components/background";
 import ModalBody from "react-bootstrap/ModalBody";
 import FeedbackCard from "./feedbackCard";
+import { useState } from "react";
 
 //TODO - possibly add css file?
 
 export const FeedbackFormsPage = () => {
+    const [currentView, setCurrentView] = useState("pending");
+
     const userQuery = useOwnUser();
     const userId = userQuery.data?.data?._id;
     const { isLoading = true, isError, data, error } = useMyShifts(userId);
@@ -26,21 +29,43 @@ export const FeedbackFormsPage = () => {
                 <ModalBody>
                     <div className="main-container">
                         <div className="tab-container">
-                            <div>Something here?</div>
+                            <div
+                                className={currentView === "pending" ? "tab active" : "tab"}
+                                onClick={() => setCurrentView("pending")}
+                            >
+                                Pending Forms
+                            </div>
+                            <div
+                                className={currentView === "completed" ? "tab active" : "tab"}
+                                onClick={() => setCurrentView("completed")}
+                            >
+                                Completed Forms
+                            </div>
                         </div>
                         <div className="content-container">
                             <div className="header-container">
                                 <h1>My Feedback Forms</h1>
                             </div>
-                            <div className="shiftList-container">
-                                {isLoading && <p>Loading feedback forms...</p>}
-                                {isError && <p>There was a server error while loading feedback forms... {error}</p>}
-                                {completedSchoolShifts &&
-                                    completedSchoolShifts.map((shift) => (
-                                        <FeedbackCard key={shift._id} userId={userId} shiftData={shift} />
-                                    ))}
-                                {!isLoading && data?.data?.length === 0 && <p>No feedback forms.</p>}
-                            </div>
+
+                            {currentView === "pending" && (
+                                <div className="pendingFormsList-container">
+                                    {isLoading && <p>Loading feedback forms...</p>}
+                                    {isError && <p>There was a server error while loading feedback forms... {error}</p>}
+                                    {completedSchoolShifts &&
+                                        completedSchoolShifts.map((shift) => (
+                                            <FeedbackCard key={shift._id} userId={userId} shiftData={shift} />
+                                        ))}
+                                    {!isLoading && data?.data?.length === 0 && <p>No feedback forms.</p>}
+                                </div>
+                            )}
+
+                            {currentView === "completed" && (
+                                <div className="completedFormsList-container">
+                                    {isLoading && <p>Loading feedback forms...</p>}
+                                    {isError && <p>There was a server error while loading feedback forms... {error}</p>}
+                                    {!isLoading && <p>No feedback forms.</p>}
+                                </div>
+                            )}
                         </div>
                     </div>
                 </ModalBody>
